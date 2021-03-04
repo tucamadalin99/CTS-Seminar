f70e2f9260a5cc904e9827e80f99e6d611322da0
TucaMadalin <maad368@gmail.com>
2021-03-04 08:23:07 +0200

    Added

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -1,14 +1,17 @@
-package ro.ase.cts.classes;
+package ro.ase.cts.main;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+
+import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.readers.AngajatReader;
 
 public class Program {
 
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
